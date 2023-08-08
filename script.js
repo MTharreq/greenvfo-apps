08ff4ebed0a274ed2a0eebe5a1c015dd5de1e699
MTharreq <mtharreq@gmail.com>
2023-08-08 23:55:42 +0700

    Menyelesaikan bagian DOM pada page utama

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const map = L.map('map').setView([0, 117.15], 10);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 //JSON Data
 const searchLayer = L.geoJSON(herbariumLocation, {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(feature.properties.Species);
-  }
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup(feature.properties.Species);
+	}
 }).addTo(map);
 
 
 //Control Search
 const searchControl = new L.Control.Search({
-  layer: searchLayer,
-  propertyName: 'Species'
+	layer: searchLayer,
+	propertyName: 'Species'
 });
 
 map.addControl(searchControl);
@@ -24,17 +24,28 @@ map.addControl(searchControl);
 
 // Event listener to access matched features after search
 searchControl.on('search:locationfound', function(event) {
-  const matchedFeature = event.layer.feature;
-  const speciesHeading = document.getElementById('species-heading');
-  const habitus = document.getElementById('habitus');
-  const distribution = document.getElementById('distribution');
-  const description = document.getElementById('description');
-  const benefit = document.getElementById('benefit');
+	const matchedFeature = event.layer.feature;
 
+	const speciesHeading = document.getElementById('species-heading');
+	const habitus = document.getElementById('habitus');
+	const distribution = document.getElementById('distribution');
+	
+	const description = document.getElementById('description');
+	const ecology = document.getElementById('ecology');
+	const benefit = document.getElementById('benefit');
+	const collector = document.getElementById('collector');
 
-  speciesHeading.textContent = matchedFeature.properties.Species;
-  habitus.textContent = matchedFeature.properties.Habitus;
-  distribution.textContent = matchedFeature.properties.Distribution;
-  description.textContent = matchedFeature.properties.Description;
-  benefit.textContent = matchedFeature.properties.Manfaat;
+	//SHOW INFOBAR
+	document.querySelector('.infobar').style.display = 'flex'
+	document.querySelector('.add-form').style.display = 'none'
+
+	//CHANGE PROPERTIES
+	speciesHeading.textContent = matchedFeature.properties.Species;
+	habitus.textContent = matchedFeature.properties.Habitus;
+	distribution.textContent = matchedFeature.properties.Distribution;
+
+	description.textContent = matchedFeature.properties.Description;
+	ecology.textContent = matchedFeature.properties.Ecology;
+	benefit.textContent = matchedFeature.properties.Manfaat;
+	collector.textContent = matchedFeature.properties.Kolektor;
 });
