6b59d9667c6a50d7413b870b208fd8db40b5b979
MTharreq <mtharreq@gmail.com>
2023-08-09 13:08:38 +0700

    Menyelesaikan keseluruhan bagian frontend pada halaman utama dan memperbaiki DOM

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-const map = L.map('map').setView([0, 117.15], 10);
-
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-//JSON Data
-const searchLayer = L.geoJSON(herbariumLocation, {
-	onEachFeature: function(feature, layer) {
-		layer.bindPopup(feature.properties.Species);
-	}
-}).addTo(map);
-
-
-//Control Search
-const searchControl = new L.Control.Search({
-	layer: searchLayer,
-	propertyName: 'Species'
-});
-
-map.addControl(searchControl);
-// console.log(herbariumLocation.features[3].properties.Species)
-// console.log(map)
-
-// Event listener to access matched features after search
-searchControl.on('search:locationfound', function(event) {
+function getInfoDOM(event){
 	const matchedFeature = event.layer.feature;
+
+	const images = document.getElementById('images')
 
 	const speciesHeading = document.getElementById('species-heading');
 	const habitus = document.getElementById('habitus');
@@ -40,6 +17,8 @@ searchControl.on('search:locationfound', function(event) {
 	document.querySelector('.add-form').style.display = 'none'
 
 	//CHANGE PROPERTIES
+	images.src = matchedFeature.properties.Image
+
 	speciesHeading.textContent = matchedFeature.properties.Species;
 	habitus.textContent = matchedFeature.properties.Habitus;
 	distribution.textContent = matchedFeature.properties.Distribution;
@@ -48,4 +27,51 @@ searchControl.on('search:locationfound', function(event) {
 	ecology.textContent = matchedFeature.properties.Ecology;
 	benefit.textContent = matchedFeature.properties.Manfaat;
 	collector.textContent = matchedFeature.properties.Kolektor;
+}
+
+
+const map = L.map('map').setView([0, 117.15], 10);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//JSON Data
+const searchLayer = L.geoJSON(herbariumLocation, {
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup(feature.properties.Species);
+	}
+}).addTo(map);
+
+// Open Infobar Overlay when click the marker
+const marker = L.geoJSON(herbariumLocation,
+	{
+		onEachFeature: function(feature, layer) {
+			layer.bindPopup(feature.properties.Species);
+		}
+	}).addTo(map)
+	.addEventListener('click', function(event) {
+		document.querySelector('.infobar').style.display = 'flex'
+		document.querySelector('.add-form').style.display = 'none'
+
+		// const matchedFeature = event.layer.feature;
+
+		getInfoDOM(event)
+
+		event.target.openPopup();
+	});
+
+//Control Search
+const searchControl = new L.Control.Search({
+	layer: searchLayer,
+	propertyName: 'Species'
+});
+
+map.addControl(searchControl);
+// console.log(herbariumLocation.features[3].properties.Species)
+// console.log(map)
+
+// Event listener to access matched features after search
+searchControl.on('search:locationfound', function(event) {
+	getInfoDOM(event)
 });
